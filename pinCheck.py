c2af812bf22d3a4083009885e657ab45d6b6c8a5
Eilidh Quinn <eilidh.mcq@gmail.com>
2022-02-06 18:05:58 +0000

    Removed prompt and updated comments/notes

--- pinCheck.py
+++ pinCheck.py
@@ -1,14 +1,14 @@
 # Pin checker with getpass - initially wasn't working
 # Had to 'Edit Configurations' then 'Emulate terminal in output console'
-# NB: correct_pin only seems to work if it is a string
-# Sun. updates: correct_pin variable, removed continue, switched order of IF statement to simplify, limited pin to 4
+# NB: correct_pin only works as a string unless I put int() before getpass.getpass, but len() wouldn't work unless str
+# Sun. updates: correct_pin variable, simplified IF statement, removed continue, limited pin to 4, removed 'prompt'
 
 import getpass
 attempts = ['2', '1', '0']
 correct_pin = '5678'
 
 for attempt in attempts:
-    pin = getpass.getpass(prompt='Enter your PIN: ')
+    pin = getpass.getpass('Enter your PIN: ')
     if len(pin) > 4:
         print("Your PIN was too long - please enter 4 characters.")
         continue
