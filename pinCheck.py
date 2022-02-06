7a54380c9ccf1ec1739d8f5c91c812fbf03eae18
Eilidh Quinn <eilidh.mcq@gmail.com>
2022-02-06 17:50:38 +0000

    Tidied code - correct_pin variable added and IF statement simplified

--- pinCheck.py
+++ pinCheck.py
@@ -1,21 +1,22 @@
-# Pin checker with getpass - nitially wasn't working - had to 'Edit Configurations' then 'Emulate terminal in output console'
+# Pin checker with getpass - initially wasn't working
+# Had to 'Edit Configurations' then 'Emulate terminal in output console'
+# NB: correct_pin only seems to work if it is a string
+# Sunday updates - made correct_pin a variable, removed continue & switched order of IF statement to simplify
+
 import getpass
 attempts = ['2', '1', '0']
-
+correct_pin = '5678'
 
 for attempt in attempts:
     pin = getpass.getpass(prompt='Enter your PIN: ')
-    if pin != '5678':
-        print('Incorrect pin. You have ' + attempt + ' attempts left.')
-        continue
-    elif pin == '5678':
+    if pin == correct_pin:
         print('Thank you, here is 1 million dollars.')
         break
+    else:
+        print('Incorrect pin. You have ' + attempt + ' attempts left.')
 
-
-# Working PIN checker without getpass module:
+# Working PIN checker without getpass module, with continue still in if statement and pin not a variable:
 # attempts = ['2', '1', '0']
-#
 #
 # for attempt in attempts:
 #     pin = input('Enter your PIN: ')
