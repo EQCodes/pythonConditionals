8c1d40710120260cbc2ede72d98c1b6d494ed481
Eilidh Quinn <eilidh.mcq@gmail.com>
2022-02-06 20:52:23 +0000

    Simplified & tidied up code

--- calculator.py
+++ calculator.py
@@ -1,12 +1,13 @@
 print("Hello! I'm Cal the Calculator.")
-begin = input("Type anything to have a go: ")
+# Edit - didn't make begin take a user input, just set as true
+#begin = input("Type anything to have a go: ")
+begin = True
 
 while begin:
-    num1 = float(input("Give me your first number: "))
-    num2 = float(input("Give me your second number: "))
-    operator = input("Give me an operator (+, -, * or /): ")
-
-
+    num1 = float(input("Enter your first number: "))
+    num2 = float(input("Enter your second number: "))
+    operator = input("Enter an operator (+, -, * or /): ")
+# Q: ^^^ is there a way to ask for a user to input just a sum e.g. 5+5, 6*7
     if operator == "+":
         print("The result is " + str(round(num1+num2, 2)))
     elif operator == "-":
@@ -16,13 +17,12 @@ while begin:
     elif operator == "/":
         print("The result is " + str(round(num1/num2, 2)))
     else:
-        print("Please input a +, -, / or * sign.")
-
-    response = input("Wanna try again? Type 'Quit' to stop or anything else to have another go: ")
-    if response != 'Quit':
+        print("Error. Please input a +, -, / or * sign.")
         continue
-    else:
-        break
 
+    goAgain = input("Wanna try again? Type 'Q' to stop or press Enter to have another go: ")
+    if goAgain == 'Q':
+         break
+    # ^^^ NB: simplified this from a full if else statement with break and continue etc.
 
 
